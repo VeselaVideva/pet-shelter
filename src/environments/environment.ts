ea35251900015c731a796dae5e5b3661085a04aa
Vesela Videva <vesela.videva@gmail.com>
2022-03-25 00:52:19 +0200

    Move Firebase config to environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pet-shelter-e3a18',
+    appId: '1:1074304291677:web:73ce829a9a2f3de1e28eb4',
+    databaseURL: 'https://pet-shelter-e3a18-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pet-shelter-e3a18.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDpo_uTP3Nn5-848Wl9shl-xBmiYnDoxi4',
+    authDomain: 'pet-shelter-e3a18.firebaseapp.com',
+    messagingSenderId: '1074304291677',
+  },
   production: false
 };
 
