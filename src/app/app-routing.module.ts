2c320f5c5f985cce13f8922ee84604824c31ea06
Vesela Videva <vesela.videva@gmail.com>
2022-03-25 19:03:59 +0200

    Add routing guards for the 3 existing pages
    When the user is logged in, can't access the Login and Sign up pages.
    All users can access the Home page.
    When the user is not logged, can't access the Add-pet page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HomeComponent } from './components/home/home.component';
 import { AddPetComponent } from "./components/add-pet/add-pet.component";
+
+const redirectToLogin = () => redirectUnauthorizedTo(['/login']);
+const redirectToHome = () => redirectLoggedInTo(['/home']);
 
 const routes: Routes = [
   {
@@ -19,15 +23,18 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    ...canActivate(redirectToHome)
   },
   {
     path: 'sign-up',
-    component: SignUpComponent
+    component: SignUpComponent,
+    ...canActivate(redirectToHome)
   },
   {
     path: 'add-pet',
-    component: AddPetComponent
+    component: AddPetComponent,
+    ...canActivate(redirectToLogin)
   }
 ];
 
