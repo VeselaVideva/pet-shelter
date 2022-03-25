8102c6f713b96976c902103132048c547a0c6872
Vesela Videva <vesela.videva@gmail.com>
2022-03-25 10:29:25 +0200

    Create Login component and add Login page

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from "../../services/auth.service";
+import { HotToastService } from "@ngneat/hot-toast";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toast: HotToastService
+  ) { }
+
+  ngOnInit(): void { }
+
+  get email() { return this.loginForm.get('email'); }
+
+  get password() { return this.loginForm.get('password'); }
+
+  submit() {
+    if (!this.loginForm.valid) { return; }
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password)
+      .pipe(
+       this.toast.observe({
+         success: 'Logged in successfully!',
+         loading: 'Logging in...',
+         error: 'There was an error while logging in!'
+       })
+      ).subscribe(() => {
+      this.router.navigate(['/home']);
+    })
+  }
+}
