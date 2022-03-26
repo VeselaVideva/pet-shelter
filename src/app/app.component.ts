978ad7d49754b17257a4059ce6d6a89c67cd23e1
Vesela Videva <vesela.videva@gmail.com>
2022-03-26 17:36:44 +0200

    Add button for testing the loader when API call is made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from "./services/auth/auth.service";
+import { LoadingService } from "./services/loading/loading.service";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,21 @@ import { AuthService } from "./services/auth/auth.service";
 })
 export class AppComponent {
   title = 'Pet Shelter';
+  loading$ = this.loader.loading$;
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    public loader: LoadingService,
+    private http: HttpClient
+  ) { }
+
+  fetchData() {
+    this.http.get('https://api.github.com/users/veselavideva')
+      .subscribe(res => {
+        console.log(res);
+      })
+  }
 
   logout() {
     this.authService.logout().subscribe(() => {
