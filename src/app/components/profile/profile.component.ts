3bbc5e05cd77480c7644558ba6da15c355c399b2
Vesela Videva <vesela.videva@gmail.com>
2022-03-27 14:36:39 +0300

    Create form in the Profile page, through which the user can add personal data

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,8 +3,12 @@ import { AuthService } from "../../services/auth/auth.service";
 import { ImageUploadService } from "../../services/upload/image-upload.service";
 import { HotToastService } from "@ngneat/hot-toast";
 import { concatMap } from "rxjs";
+import { FormControl, FormGroup } from '@angular/forms';
+import { UsersService } from "../../services/users/users.service";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { UserProfile } from "../../models/user-profile";
 
-
+@UntilDestroy()
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,17 +16,33 @@ import { concatMap } from "rxjs";
 })
 export class ProfileComponent implements OnInit {
 
-  user$ = this.authService.currentUser$;
+  user$ = this.usersService.currentUserProfile$;
+
+  profileForm = new FormGroup({
+    uid: new FormControl(''),
+    displayName: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phone: new FormControl(''),
+    address: new FormControl('')
+  });
 
   constructor(
     private authService: AuthService,
     private ImageUploadService: ImageUploadService,
-    private toast: HotToastService
+    private toast: HotToastService,
+    private usersService: UsersService
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.usersService.currentUserProfile$
+      .pipe(untilDestroyed(this))
+      .subscribe((user) => {
+      this.profileForm.patchValue({ ...user });
+    })
+  }
 
-  uploadImage(event: any, user: any) {
+  uploadImage(event: any, user: UserProfile) {
     this.ImageUploadService
       .uploadImage(event.target.files[0], `images/profile/${user.uid}`)
       .pipe(
@@ -31,7 +51,20 @@ export class ProfileComponent implements OnInit {
           loading: 'Image is being uploaded...',
           error: 'There was an error while uploading!'
         }),
-        concatMap((photoURL: any) => this.authService.updateProfileData({ photoURL }))
+        concatMap((photoURL: any) => this.usersService.updateUser({ uid: user.uid, photoURL }))
+      ).subscribe();
+  }
+
+  saveProfile() {
+    const profileData = this.profileForm.value;
+    this.usersService
+      .updateUser(profileData)
+      .pipe(
+        this.toast.observe({
+          loading: 'Updating data...',
+          success: 'Data has been updated!',
+          error: 'There was an error while updating the data!'
+        })
       ).subscribe();
   }
 }
