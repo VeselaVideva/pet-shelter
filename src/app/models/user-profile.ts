1105ce62fb714102cfd8a5fda6eabee4250a93f7
Vesela Videva <vesela.videva@gmail.com>
2022-03-27 14:34:21 +0300

    Create UserProfile interface

---
+++ src/app/models/user-profile.ts
@@ -0,0 +1,10 @@
+export interface UserProfile {
+  uid: string;
+  email?: string;
+  displayName?: string;
+  photoURL?: string;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+  address?: string;
+}
