c3b1a499adc5d40bf9fb21d959663f0cc8b9df7e
Vesela Videva <vesela.videva@gmail.com>
2022-03-27 12:57:18 +0300

    Print some data from the realtime database on the Home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { RealtimeDbService } from "../../services/realtime-db/realtime-db.service";
+import { map } from "rxjs";
+import { Pet } from "../../models/pet.model";
 
 @Component({
   selector: 'app-home',
@@ -9,9 +12,27 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class HomeComponent implements OnInit {
 
   user$ = this.authService.currentUser$;
+  pets?: Pet[];
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private dbService: RealtimeDbService
+  ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.retrievePets();
+  }
 
+  retrievePets(): void {
+    this.dbService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      this.pets = data;
+      console.log(data);
+    });
+  }
 }
--- src/app/models/pet.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Pet } from './pet.model';
-
-describe('Pet', () => {
-  it('should create an instance', () => {
-    expect(new Pet()).toBeTruthy();
-  });
-});
