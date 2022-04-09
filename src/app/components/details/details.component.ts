578485bc6112530bd70bb81ad2c76e06af65589e
Vesela Videva <vesela.videva@gmail.com>
2022-04-10 01:39:54 +0300

    Implement Edit and Delete functionality for the pet fosterer (user who added the pet to database)

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 import { Pet } from "../../models/pet.model";
+import { dbPath } from "../../../environments/environment";
 import { RealtimeDbService } from "../../services/realtime-db/realtime-db.service";
 import { UsersService } from "../../services/users/users.service";
 import { LoadingService } from "../../services/loading/loading.service";
-import { ActivatedRoute } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
-import { dbPath } from "../../../environments/environment";
+
 
 @Component({
   selector: 'app-details',
@@ -19,26 +21,36 @@ export class DetailsComponent implements OnInit {
   pets?: Pet[];
 
   @Input() pet?: Pet;
-  @Output() refreshList: EventEmitter<any> = new EventEmitter();
 
   currentPet: Pet = {
     photoURL: 'string',
-    name: 'string',
     age: 0,
-    breed: 'string',
     description: 'string'
   };
   message = '';
+  editActive = false;
+
+  createForm = new FormGroup({
+    photoURL: new FormControl('', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]),
+    age: new FormControl('', [Validators.required, Validators.min(0)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(50)])
+  });
 
   constructor(
     private dbService: RealtimeDbService,
     private usersService: UsersService,
     public loader: LoadingService,
     private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
+  get photoURL() { return this.createForm.get('photoURL'); }
+  get age() { return this.createForm.get('age'); }
+  get description() { return this.createForm.get('description'); }
+
   ngOnInit(): void {
+    this.message = '';
     this.retrievePet(this.activatedRoute.snapshot.params['key']);
   }
 
@@ -54,29 +66,38 @@ export class DetailsComponent implements OnInit {
       })
   }
 
-  updatePet(): void { // TODO
-    const data = {
-      photoURL: this.currentPet.photoURL,
-      name: this.currentPet.name,
-      age: this.currentPet.age,
-      description: this.currentPet.description
-    };
+  showEditForm(): void {
+    this.editActive = true;
+  }
 
-    if (this.currentPet.key) {
-      this.dbService.update(this.currentPet.key, data)
-        .then(() => this.message = 'Pet info was updated successfully!')
+  hideEditForm(): void {
+    this.editActive = false;
+  }
+
+  updatePet(): void {
+    if (!this.createForm.valid) { return; }
+
+    this.currentPet = this.createForm.value; //console.log(this.currentPet);
+
+    if (this.activatedRoute.snapshot.params['key']) {
+      this.dbService.update(this.activatedRoute.snapshot.params['key'], this.currentPet)
+        .then(() => {
+          this.message = 'Pet info was updated successfully!'
+          this.router.navigate(['/home']);
+        })
         .catch(err => console.log(err));
     }
   }
 
-  deletePet(): void { // TODO
-    if (this.currentPet.key) {
-      this.dbService.delete(this.currentPet.key)
-        .then(() => {
-          this.refreshList.emit();
-          this.message = 'This pet was removed!';
-        })
-        .catch(err => console.log(err));
+  deletePet(): void {
+    if(confirm(`Are you sure you want to remove this pet from the shelter database? Caution: This action is irreversible!`)) {
+      if (this.activatedRoute.snapshot.params['key']) {
+        this.dbService.delete(this.activatedRoute.snapshot.params['key'])
+          .then(() => {
+            this.router.navigate([`/home`]);
+          })
+          .catch(err => console.log(err));
+      }
     }
   }
 }
